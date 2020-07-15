efc7d40a21880fbf91baa1393141ff8c3dbbae9d
Guilherme <trintaeoitogc@gmail.com>
2020-07-14 22:56:27 -0300

    finish

--- factorization.cpp
+++ factorization.cpp
@@ -2,18 +2,47 @@
 	#define OS_Windows
 #endif
 
+#include<time.h>
+#include<string>
 #include<iostream>
+
 using std::cout;
 using std::cin;
 using std::endl;
+using std::string;
 
-int fatora(int x) {
+int factorUsingRecursion(int x) {
 	if (x == 1){
 		return 1;
 	}
 	else {
-		return x * fatora(x - 1);
+		return x * factorUsingRecursion(x - 1);
 	}
+}
+
+int factorUsingFor(int x) {
+	int result = 1;
+
+	for ( int i=x; i>0; i-- ) {
+		result *= i;	
+	}	
+
+	return result;
+}
+
+void clearScreen() {
+	#ifdef OS_Windows
+		system("cls");
+	#else
+		system("clear");
+	#endif
+}
+
+void printResult(int x, int result, float diff, string method) {
+	cout << method << endl;
+	cout << x << "! = " << result << endl;
+	cout << "runtime: " << diff << " milliseconds" << endl;
+	cout << endl;
 }
 
 int main() {
@@ -22,11 +51,21 @@ int main() {
 	cout << ": ";
 	cin >> x;
 
-	#ifdef OS_Windows
-		system("cls");
-	#else
-		system("clear");
-	#endif
+	float diff;
+	clock_t t_start, t_end;
 
-	cout << x << "! = " << fatora(x) << endl;
+	clearScreen();
+
+	t_start = clock();
+	int factorRecursion = factorUsingRecursion(x);		
+	t_end = clock();
+	diff =  t_end - t_start;
+	printResult(x, factorRecursion, diff, "recursion");	
+	
+	t_start = clock();
+	int factorFor = factorUsingFor(x);		
+	t_end = clock();
+	diff =  t_end - t_start;
+	printResult(x, factorFor, diff, "for");
+	
 }
